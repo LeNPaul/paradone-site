3049904b3dbbf6c184566e3e97f46a146bc0b4e7
Paul Le <l.nguyen.paul@gmail.com>
2020-08-15 16:37:56 -0400

    Remove all configuration files except IguanaMainRepo and IguanaEnv.txt file

--- iguana-wk1-config/run/Poller_Manager-From-A8ZZApgTjifrxu/shared/ha/conf.lua
+++
@@ -1,3 +0,0 @@
-return  {
-   auth = { username = "admin", password = "password" }
-}
--- iguana-wk2-config/run/Test_Poller-From-7hiwwAYoUXWdjF/Test_Poller-From-7hiwwAYoUXWdjF/main.lua
+++
@@ -1,4 +0,0 @@
--- The main function is the first function called from Iguana.
-function main()
-   queue.push{data='Test polling message'}
-end
